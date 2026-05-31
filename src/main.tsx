da66e061e43fb3b9220f8cdfda6f4937b8e04744
Lorenz F. Estrella <lorenzstrll@gmail.com>
2026-05-31 22:39:56 +0800

    fix: link global index.css entrypoint to restore tailwind styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
+import './index.css' // <-- ADD THIS EXACT LINE RIGHT HERE!
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
